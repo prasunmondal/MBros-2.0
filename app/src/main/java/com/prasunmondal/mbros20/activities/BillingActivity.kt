329889efc9b909481cd5d1f0c728dc186f859b44
Prasun Mondal <prasun.mondal@oracle.com>
2021-01-16 19:03:09 +0530

    show Billing details

--- app/src/main/java/com/prasunmondal/mbros20/activities/BillingActivity.kt
+++ app/src/main/java/com/prasunmondal/mbros20/activities/BillingActivity.kt
@@ -3,32 +3,50 @@ package com.prasunmondal.mbros20.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import com.prasunmondal.mbros20.R
+import com.prasunmondal.mbros20.calculators.Calculator
 import com.prasunmondal.mbros20.models.Delivery
 import com.prasunmondal.mbros20.models.Order
+import com.prasunmondal.mbros20.models.OrderList
+import java.lang.Exception
 
 class BillingActivity : AppCompatActivity() {
+
+    private lateinit var textViewTodaysAmount: TextView
+    private lateinit var textViewPreviousAmount: TextView
+    private lateinit var textViewTotalAmount: TextView
+    private lateinit var editTextTodaysPayment: EditText
+    private lateinit var textViewRemainingAmount: TextView
+
+    private lateinit var order: Order
+    private lateinit var deliveryObject: Delivery
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_billing)
+
+        textViewTodaysAmount = findViewById(R.id.billing_todaysAmount)
+        textViewPreviousAmount = findViewById(R.id.billing_prevDue)
+        textViewTotalAmount = findViewById(R.id.billing_totalAmount)
+        editTextTodaysPayment = findViewById(R.id.billing_todaysPayment)
+        textViewRemainingAmount = findViewById(R.id.billing_remainingAmount)
 
         Log.d("Billing Activity: ", "onCreate")
         val bundle = intent.extras
         if(bundle != null) {
             Log.d("Billing Activity: ", "bundle is not null")
-            val deliveryObject: Delivery? = bundle.getSerializable("deliveryObject") as Delivery?
-            if (deliveryObject != null) {
-                Log.d("Billing Activity: ", "delivery object is not null")
-                prepareBillingUI(deliveryObject)
-            } else {
-                Log.d("Billing Activity: ", "delivery object is null")
-            }
+            deliveryObject = bundle.getSerializable("deliveryObject")!! as Delivery
+            Log.d("Billing Activity: ", "delivery object is not null")
+            order = OrderList.instance.getOrderByCustomerId(deliveryObject.customerId)!!
+            prepareBillingUI()
         } else {
             Log.d("Billing Activity: ", "bundle is null")
         }
     }
 
-    private fun prepareBillingUI(deliveryObject: Delivery) {
+    private fun prepareBillingUI() {
         Log.d("Billing Activity: ",
             deliveryObject.kilos + ", "
                     + deliveryObject.pcs + ", "
@@ -36,5 +54,28 @@ class BillingActivity : AppCompatActivity() {
                     + deliveryObject.pricePerKilo + ", "
                     + deliveryObject.todayPaid + ", "
                     + deliveryObject.customerName + ", ")
+
+        textViewTodaysAmount.text = Calculator.deliveryTotalPriceForTodaysDelivery(
+            deliveryObject, order).toString()
+
+        textViewPreviousAmount.text = OrderList.instance.getOrderByCustomerId(deliveryObject.customerId)!!.previousDue
+
+        textViewTotalAmount.text = Calculator.deliveryTotalAmount_today_prevDue(deliveryObject,
+            OrderList.instance.getOrderByCustomerId(deliveryObject.customerId)!!).toString()
+
+        setRemainingAmount()
+    }
+
+    private fun setRemainingAmount() {
+        textViewRemainingAmount.text = Calculator.deliveryRemainingAmount(deliveryObject,
+            OrderList.instance.getOrderByCustomerId(deliveryObject.customerId)!!, getPaidAmount()).toString()
+    }
+
+    private fun getPaidAmount(): Float {
+        return try {
+            editTextTodaysPayment.text.toString().toFloat()
+        } catch (e: Exception) {
+            0.0F
+        }
     }
 }
---
+++ app/src/main/java/com/prasunmondal/mbros20/calculators/Calculator.kt
@@ -0,0 +1,44 @@
+package com.prasunmondal.mbros20.calculators
+
+import com.prasunmondal.mbros20.activities.Denominations
+import com.prasunmondal.mbros20.models.Delivery
+import com.prasunmondal.mbros20.models.Order
+import java.lang.Exception
+
+class Calculator {
+
+    companion object {
+        fun deliveryTotalKg(delivery: Delivery): Float {
+            var kg = 0.0F
+
+            delivery.pc_kilo_denominations.forEach { denomination ->
+                kg += denomination.kg.toFloat()
+            }
+            return kg
+        }
+
+        fun deliveryTotalPc(delivery: Delivery): Int {
+            var pc = 0
+
+            delivery.pc_kilo_denominations.forEach { denomination ->
+                pc += denomination.kg.toInt()
+            }
+            return pc
+        }
+
+        fun deliveryTotalPriceForTodaysDelivery(delivery: Delivery, order: Order): Float {
+            return (order.pricePerKilo.toFloat() * deliveryTotalKg(delivery))
+        }
+
+        fun deliveryTotalAmount_today_prevDue(delivery: Delivery, order: Order): Float {
+            return deliveryTotalPriceForTodaysDelivery(
+                delivery,
+                order
+            ) + order.previousDue.toFloat()
+        }
+
+        fun deliveryRemainingAmount(delivery: Delivery, order: Order, paidAmount: Float): Float {
+            return deliveryTotalAmount_today_prevDue(delivery, order) - paidAmount
+        }
+    }
+}
