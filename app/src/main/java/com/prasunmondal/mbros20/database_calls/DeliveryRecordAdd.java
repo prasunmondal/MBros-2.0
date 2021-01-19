3003378c77f369c2c609ce1ce9c93d9f0f2031cd
Prasun Mondal <prasun.mondal@oracle.com>
2021-01-19 12:53:21 +0530

    failing

--- app/src/main/java/com/prasunmondal/mbros20/database_calls/DeliveryRecordAdd.java
+++ app/src/main/java/com/prasunmondal/mbros20/database_calls/DeliveryRecordAdd.java
@@ -1,5 +1,9 @@
 package com.prasunmondal.mbros20.database_calls;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.prasunmondal.mbros20.database_utils.UpdateDataInDB;
 import com.prasunmondal.mbros20.utils.StringConstants;
 import com.prasunmondal.mbros20.models.Delivery;
@@ -10,16 +14,26 @@ public class DeliveryRecordAdd
 {
     public void execute(Delivery delivery, Consumer<String> onSuccess, Consumer<String> onFailure) throws Exception
     {
+        GsonBuilder builder = new GsonBuilder();
+        builder.disableInnerClassSerialization();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        String jsonString2 = gson.toJson(delivery);
+        Log.d("jsonObject: ", jsonString2);
+
         UpdateDataInDB sd = new UpdateDataInDB(
                 getStringToSave(delivery),
                 DatabaseStrings.TABNAME_DELIVERY,
                 delivery.getOrderId(),
                 0,
-                jsonString -> onComplete(jsonString, onSuccess, onFailure));
+                jsonString -> onComplete(jsonString, onSuccess, onFailure),
+                jsonString2);
         sd.execute();
     }
 
     private static String getStringToSave(Delivery delivery) {
+//        Delivery student = Gson.fromJson(jsonString, Student.class)
+
         return delivery.getOrderId() + StringConstants.SEPARATOR_DB
                 + delivery.getDeliveryId() + StringConstants.SEPARATOR_DB
                 + delivery.getDeliveryDate() + StringConstants.SEPARATOR_DB
--- app/src/main/java/com/prasunmondal/mbros20/database_calls/OrderAdd.java
+++ app/src/main/java/com/prasunmondal/mbros20/database_calls/OrderAdd.java
@@ -4,18 +4,20 @@ import com.prasunmondal.mbros20.database_utils.UpdateDataInDB;
 import com.prasunmondal.mbros20.models.Order;
 import com.prasunmondal.mbros20.utils.StringConstants;
 
+import org.json.JSONObject;
+
 import java.util.function.Consumer;
 
 public class OrderAdd
 {
-    public void execute(Order order, Consumer<String> onCompletion) throws Exception
+    public void execute(Order order, Consumer<String> onCompletion, String json) throws Exception
     {
         UpdateDataInDB sd = new UpdateDataInDB(
                 getStringToSave(order),
                 DatabaseStrings.TABNAME_ORDER,
                 order.customerId,
                 0,
-                onCompletion);
+                onCompletion, json);
         sd.execute();
     }
 
